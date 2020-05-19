801597ddf4029ac78d8f5a1f639f97a3b970fff3
beeth0ven <beeth0vendev@gmail.com>
2020-05-19 22:20:56 +0800

    add basic widgets

--- lib/src/widgets_basic_chain.dart
+++ lib/src/widgets_basic_chain.dart
@@ -5,6 +5,30 @@ import 'core.dart';
 
 extension WidgetsBasic on Chain {
 
+  Chain align({
+    Key key,
+    AlignmentGeometry alignment = Alignment.center,
+    double widthFactor,
+    double heightFactor,
+  }) =>
+    this.add((context, child) => Align(
+      key: key,
+      alignment: alignment,
+      widthFactor: widthFactor,
+      heightFactor: heightFactor,
+      child: child
+    ));
+
+  Chain aspectRatio({
+    Key key,
+    double aspectRatio,
+  }) =>
+    this.add((context, child) => AspectRatio(
+      key: key,
+      aspectRatio: aspectRatio,
+      child: child
+    ));
+
   Chain center({ 
     Key key, 
     double widthFactor, 
@@ -68,5 +92,151 @@ extension WidgetsBasic on Chain {
       textBaseline: textBaseline,
       children: children
     ));
+
+  Chain constrainedBox({
+    Key key,
+    BoxConstraints constraints,
+  }) =>
+    this.add((context, child) => ConstrainedBox(
+      key: key,
+      constraints: constraints,
+      child: child
+    ));
+
+  Chain expanded({
+    Key key,
+    int flex = 1,
+  }) =>
+    this.add((context, child) => Expanded(
+      key: key,
+      flex: flex,
+      child: child
+    ));
+
+  Chain ignorePointer({
+    Key key, 
+    bool ignoring, 
+    bool ignoringSemantics,
+  }) =>
+    this.add((context, child) => IgnorePointer(
+      key: key,
+      ignoring: ignoring,
+      ignoringSemantics: ignoringSemantics,
+      child: child
+    ));
   
+  Chain opacity({
+    Key key,
+    @required double opacity,
+    bool alwaysIncludeSemantics = false,
+  }) =>
+    this.add((_, child) => Opacity(
+      key: key,
+      opacity: opacity,
+      alwaysIncludeSemantics: alwaysIncludeSemantics,
+      child: child
+    ));
+
+  Chain padding({
+    Key key,
+    @required EdgeInsetsGeometry padding,
+  }) =>
+    this.add((context, child) => Padding(
+      key: key,
+      padding: padding,
+      child: child
+    ));
+
+  Chain row({
+    Key key,
+    MainAxisAlignment mainAxisAlignment = MainAxisAlignment.start,
+    MainAxisSize mainAxisSize = MainAxisSize.max,
+    CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.center,
+    TextDirection textDirection,
+    VerticalDirection verticalDirection = VerticalDirection.down,
+    TextBaseline textBaseline,
+    List<Widget> children = const <Widget>[],
+  }) =>
+    this.add((_, __) => Row(
+      key: key,
+      mainAxisAlignment: mainAxisAlignment,
+      mainAxisSize: mainAxisSize,
+      crossAxisAlignment: crossAxisAlignment,
+      textDirection: textDirection,
+      verticalDirection: verticalDirection,
+      textBaseline: textBaseline,
+      children: children
+    ));
+
+  Chain sizedBox({
+    Key key,
+    double width, 
+    double height,
+  }) =>
+    this.add((context, child) => SizedBox(
+      key: key,
+      width: width, 
+      height: height,
+      child: child
+    ));
+
+  Chain sizedBoxExpand({
+    Key key,
+  }) =>
+    this.add((context, child) => SizedBox.expand(
+      key: key,
+      child: child
+    ));
+
+  Chain sizedBoxShrink({
+    Key key,
+  }) =>
+    this.add((context, child) => SizedBox.shrink(
+      key: key,
+      child: child
+    ));
+
+  Chain sizedBoxFromSize({
+    Key key,
+    Size size,
+  }) =>
+    this.add((context, child) => SizedBox.fromSize(
+      key: key,
+      size: size,
+      child: child
+    ));
+
+  Chain text(
+    String data, {
+    Key key, 
+    TextStyle style,
+    StrutStyle strutStyle, 
+    TextAlign textAlign, 
+    TextDirection textDirection, 
+    Locale locale, 
+    bool softWrap, 
+    TextOverflow overflow, 
+    double textScaleFactor, 
+    int maxLines, 
+    String semanticsLabel, 
+    TextWidthBasis textWidthBasis, 
+    TextHeightBehavior textHeightBehavior
+  }) =>
+    this.add((context, child) => Text(
+      data,
+      key: key,
+      style: style,
+      strutStyle: strutStyle, 
+      textAlign: textAlign, 
+      textDirection: textDirection, 
+      locale: locale, 
+      softWrap: softWrap, 
+      overflow: overflow, 
+      textScaleFactor: textScaleFactor, 
+      maxLines: maxLines, 
+      semanticsLabel: semanticsLabel, 
+      textWidthBasis: textWidthBasis, 
+      textHeightBehavior: textHeightBehavior
+    ));
+
 }
